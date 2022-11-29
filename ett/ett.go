ccd3906b7560ddee57d0c9a95e7764c645a2451d
ZZHeng <46418143+zzheng2020@users.noreply.github.com>
2022-11-29 16:59:10 +0100

    finish Route Group Control

--- ett/ett.go
+++ ett/ett.go
@@ -1,18 +1,56 @@
 package ett
 
 import (
+	"log"
 	"net/http"
 )
 
 // define handler to handle request
 type HandlerFunc func(*Context)
 
-type Engine struct {
-	router *router
-}
+type (
+	RouterGroup struct {
+		prefix       string
+		middleweares []HandlerFunc
+		parent       *RouterGroup
+		engine       *Engine
+	}
+	Engine struct {
+		*RouterGroup
+		router *router
+		groups []*RouterGroup
+	}
+)
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{
+		router: newRouter(),
+	}
+
+	engine.RouterGroup = &RouterGroup{
+		engine: engine,
+	}
+
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
+}
+
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
 // handle received requests
@@ -21,12 +59,12 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	engine.router.handle(c)
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.router.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
--- main.go
+++ main.go
@@ -7,25 +7,34 @@ import (
 
 func main() {
 	r := ett.New()
-
-	r.GET("/", func(ctx *ett.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello Ett</h1>")
+	r.GET("/index", func(c *ett.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
-
-	r.GET("/hello", func(ctx *ett.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
-	})
-
-	r.GET("/hello/:name", func(ctx *ett.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Param("name"), ctx.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *ett.Context) {
-		c.JSON(http.StatusOK, ett.H{
-			"filepath": c.Param("filepath"),
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *ett.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello ett</h1>")
 		})
-	})
+
+		v1.GET("/hello", func(c *ett.Context) {
+			// expect /hello?name=ettktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *ett.Context) {
+			// expect /hello/ettktutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *ett.Context) {
+			c.JSON(http.StatusOK, ett.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
-
 }
