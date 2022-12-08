e2263a6e180bbdcb192b35eb4883c412ec5deddf
ZZHeng <46418143+zzheng2020@users.noreply.github.com>
2022-12-09 00:17:28 +0100

    add lru cache (will support concurrency later)

---
+++ ettcache/lru/lru.go
@@ -0,0 +1,83 @@
+package lru
+
+import (
+	"container/list"
+	"fmt"
+)
+
+/*
+* Least Recently Used, LRU
+ */
+
+/*
+* maxBytes: maximum memory allowed.
+* nbytes: currently used memory
+ */
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	ll        *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+/*
+* Len(): return the memory size occupied by the return value
+ */
+type Value interface {
+	Len() int
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		ll:        list.New(),
+		cache:     make(map[string]*list.Element),
+		OnEvicted: onEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if ele, ok := c.cache[key]; ok {
+		// Agreed that the front is the tail of the queue
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+func (c *Cache) RemoveOldest() {
+	ele := c.ll.Back()
+	if ele != nil {
+		c.ll.Remove(ele)
+		kv := ele.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value Value) {
+	if ele, ok := c.cache[key]; ok {
+		c.ll.MoveToFront(ele)
+		kv := ele.Value.(*entry)
+		fmt.Println("kv:", kv)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		ele := c.ll.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
--- go.mod
+++ go.mod
@@ -3,5 +3,7 @@ module gf
 go 1.19
 
 require ett v0.0.0
+require ettcache v0.0.0
 
 replace ett => ./ett
+replace ettcache => ./ettcache
